6f936d43770a18541a2c80853dd90efbe1558528
Andrey-Shushkanov <shushkanovandrey@mail.ru>
2024-06-09 21:25:40 +0500

    Initial commit

---
+++ module2_2.py
@@ -0,0 +1,9 @@
+first = int(input('Введите первое число: '))
+second = int(input('Введите второе число: '))
+third = int(input('Введите третье число: '))
+if first == second and first == third:
+    print('3 одинаковых числа')
+elif first == second or first == third or second == third:
+    print('2 одинаковых числа')
+else:
+    print('Совпадающих чисел нет')
